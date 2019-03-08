36aad57bdffb736e73b32243794b4899458a8137
introoctave <grupo.moccai@gmail.com>
2019-03-08 17:29:22 -0300

    mejora sección scripts de clases

--- clases_archivos/brazotensionado_torque.m
+++ clases_archivos/brazotensionado_torque.m
@@ -1,20 +1,20 @@
-% Ejercicio: mecánica vectorial
+% Ejercicio: mecï¿½nica vectorial
 %
-% Una estructura rígida con forma de L (brazo) se carga en el extremo libre 
+% Una estructura rï¿½gida con forma de L (brazo) se carga en el extremo libre 
 % mediante un tensor como se muestra en la figura (ver apuntes de clase).
-% La tensión en el cable se puede ajustar hasta un valor máximo de 2.5kN. 
+% La tensiï¿½n en el cable se puede ajustar hasta un valor mï¿½ximo de 2.5kN. 
 %
 % Determinar, con OCTAVE, el momento M=r x T respecto al punto O que produce
-% la tensión del cable, que actúa en el punto A, y la magnitud de ese momento.
+% la tensiï¿½n del cable, que actï¿½a en el punto A, y la magnitud de ese momento.
 %
-% Se desea conocer como afecta la magnitud del momento torsor M la posición B
+% Se desea conocer como afecta la magnitud del momento torsor M la posiciï¿½n B
 % sobre el plano xy, en la cual es fijado el cable si se mantiene constante la
 % longitud del cable.
 %
 % 1) Grafique el sistema que debe ser analizado
 % 2) Determine las posiciciones de B en las que se logra el min(M) y max(M).
-% 3) Grafique la norma de M en función del ángulo alfa=[0,2pi] que parametriza
-%    la posición de B en el plano xy.
+% 3) Grafique la norma de M en funciï¿½n del ï¿½ngulo alfa=[0,2pi] que parametriza
+%    la posiciï¿½n de B en el plano xy.
 
 %coordenadas del "brazo" donde se fija el tensor
 brazo = [0.0,0.0,0.0; 
@@ -25,7 +25,7 @@ brazo = [0.0,0.0,0.0;
 % El punto B se fija a una rueda de radio R=sqrt(1.3^2+0.7^2)
 % ubicada en el plano xy (z=0).
 % Definimos las posiciones del punto B como aquellos sobre un 
-% círculo de radio R parametrizadas por alfa en [0,2pi];
+% cï¿½rculo de radio R parametrizadas por alfa en [0,2pi];
 
 Rten=sqrt(0.7^2+1.3^2);
 alfa=linspace(0,2*pi,360)';
@@ -38,8 +38,11 @@ auxP=[1.7,0.0,2.1;
       2.4,1.3,0.0];
       
 %=====================================================
-% A continuación se representa gráficamente el problema bajo
+% A continuaciï¿½n se representa grï¿½ficamente el problema bajo
 % estudio.
+nB=[tenB(2,1)-tenB(1,1), tenB(2,2)-tenB(1,2), tenB(2,3)-tenB(1,3)];
+nB=nB/norm(nB);
+
 figure(1);clf
 hold on
 plot3(brazo(:,1),brazo(:,2),brazo(:,3),'ko-','markersize',12,'linewidth',8)
@@ -47,14 +50,16 @@ plot3( tenB(:,1), tenB(:,2), tenB(:,3),'bo-','markersize',12,'linewidth',4)
 plot3( posB(:,1), posB(:,2), posB(:,3),'r--','linewidth',4)
 plot3( posB(1,1), posB(1,2), posB(1,3),'rs','linewidth',4,'markersize',12)
 plot3( auxP(:,1), auxP(:,2), auxP(:,3),'k--o','linewidth',3)
+quiver3(tenB(1,1), tenB(1,2), tenB(1,3), nB(1), nB(2), nB(3), 'linewidth',4,'markersize',12)
 hold off
 text( 0.2,-0.2, 0.2,'O','fontsize',24)
 text( 1.7, 0.0, 2.3,'A','fontsize',24)
 text( 2.4, 1.3, 0.2,'B','fontsize',24)
-xlabel('x','fontsize',20)
-ylabel('y','fontsize',20)
-zlabel('z','fontsize',20)
-set(gca,'fontsize',20)
+text( mean(tenB(:,1)), mean(tenB(:,2)), mean(tenB(:,3)),'T','fontsize',24)
+xlabel('x [m]','fontsize',20)
+ylabel('y [m]','fontsize',20)
+zlabel('z [m]','fontsize',20)
+set(gca,'fontsize',20,'linewidth',1)
 grid
 box off
 axis equal
@@ -62,7 +67,7 @@ xlim([-0.1,3.2])
 ylim([-1.6,1.6])
 zlim([-0.1,2.3])
 fig1 = gcf ();
-set(fig1, "numbertitle", "off", "name", "Mecánica Vectorial")
+set(fig1, "numbertitle", "off", "name", "MecÃ¡nica Vectorial")
 
 %Momento torsor en O realizado por la fuera T en 3 posiciones de B
 rA=[1.7,0.0,2.1];
@@ -80,7 +85,7 @@ nM3=norm(cross(rA,T3))
 % el cable tensor, T, parametrizado por el angulo alfa que determina
 % la posicion del punto B en el plano xy.
 
-T = 2.5; %norma de la tension en el cable
+T = 2.5; %norma de la tension en el cable en kN
 Mnorm=zeros(360,1);
 
 for i=1:360
@@ -90,8 +95,11 @@ for i=1:360
   Mnorm(i) = norm(MOA);
 end
 
-figure(1);clf
+figure(2);clf
 plot(alfa, Mnorm, 'r-', 'linewidth', 4)
-xlabel('\alpha','fontsize',24)
-ylabel('M(\alpha)','fontsize',24)
-set(gca,'fontsize',20)
+xlabel('\alpha [rad]','fontsize',24)
+ylabel('|M_O| [kJ]','fontsize',24)
+xlim([0,2*pi])
+set(gca,'fontsize',20,'linewidth',1,'xtick',[0,pi,2*pi],'xticklabel',['0';'\pi'; '2\pi'])
+fig2 = gcf ();
+set(fig2, "numbertitle", "off", "name", "Norma Momento Torsor en O")
