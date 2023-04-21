50d5cb1869c32c6c083d8b37ae84bbc2ede2c33d
Daniel Millán <rdaniel.millan@gmail.com>
2023-04-21 13:01:47 -0300

    Curso 2023. TP1.

---
+++ practicas_archivos/scripts/TP1_Ej7_CurvaPlana.m
@@ -0,0 +1,22 @@
+% Introduccion a Octave - 2023 - FCAI-UNCuyo
+% 
+%
+%% Guia de problemas 1
+%% 
+%% Modifique el siguiente script, para obtener el grafico que se muestra en el 
+%% trabajo practico. 
+%% Ayuda: prueba y error suele funcionar. Intente relacionar el comando con la figura, 
+%% y vuelva a ejecutar luego de cada cambio en el script.
+
+figure(1);clf %% le da un numero a la figura
+hold on
+ez1 = ezplot("sqrt(1-x^2)/2",[-1,1])  %% primera figura
+ez2 = ezplot("x^2/2",[-1,1])          %% segunda figura
+set(ez1,'color',[0 0 0],'linewidth',2)
+set(ez2,'color',[0 0 0],'linewidth',2)  %% Ayuda: [R G B]
+hold off
+title(' ','fontsize',14)       
+xlabel(' ','fontsize',14)
+ylabel('  ','fontsize',14)
+axis([-1.1,1.1,-0.1,0.6])
+axis equal
---
+++ practicas_archivos/scripts/TP1_Ej8_EzAlgo.m
@@ -0,0 +1,24 @@
+% Introduccion a Octave - 2023 - FCAI-UNCuyo
+% 
+%
+%% Guia de problemas 1
+
+%% El script esta listo para ejecutarlo. Relacione la figura con el comando correspondiente.
+
+figure(1)
+ezplot3('t/2*cos(t)','t/2*sin(t)','t')
+
+figure(2)
+ezsurf('sin (sqrt (x^2 + y^2)) / (sqrt (x^2 + y^2))')
+
+figure(3)
+ezsurf('x^2 + y^2', [-1, 1])
+
+figure(4)
+ezsurf('sin(x)*cos(y)',[-pi, pi])
+
+figure(5)
+ezsurf('sin(x^2 + y^2)', [-pi/2, pi/2])
+
+figure(6)
+ezcontour('sin(x^2 + y^2)', [-pi/2, pi/2])
