70009db6d1187fef91aaf6c968a314b61c0ccac2
Nicolas Muzi <nmuzi@fcai.uncu.edu.ar>
2023-06-02 09:58:09 -0300

    Agregada clase 4 y scripts de la clase

---
+++ clases_archivos/U4_ej_if_else.m
@@ -0,0 +1,20 @@
+% Introduccion a Octave - 2023 - FCAI-UNCuyo -  Depto Ingenieria Mecanica
+% Grupo MoCCAI: Modelado Computacional en Ciencias Aplicadas e Ingenieria
+%
+% |Unidad 4|
+% Ejemplo: if-elseif-else 
+
+% generamos un número aleatorio entre 1 y 10
+x = ceil(rand(1,1)*10);
+
+% se imprime en la terminal de comandos el nro generado
+printf("\tVariable x=%d\n",x);
+
+% se ejecuta la "Sentencia" de acuerdo a alguna "Condicion"
+if (x==1)
+	disp("Variable is 1")
+elseif (x==6 || x==7)
+	disp("Variable is either 6 or 7")
+else
+	disp("Variable is neither 1, 6 nor 7")
+end
